17928d8864c48d44f14a6ee8cf3292aa7c880d16
515orestis <orestis.gounalakis@thinksocial.limited>
2023-05-29 04:56:56 +0300

    restore

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -70,10 +70,10 @@
  */
 #define WEBSITE_URL "github.com/mriscoc/Ender3V2S1/wiki"
 
-                                       /**
-                                        * Set the vendor info the serial USB interface, if changable
-                                        * Currently only supported by DUE platform
-                                        */
-                                       // #define USB_DEVICE_VENDOR_ID           0x0000
-                                       // #define USB_DEVICE_PRODUCT_ID          0x0000
-                                       // #define USB_DEVICE_MANUFACTURE_NAME    WEBSITE_URL
+/**
+ * Set the vendor info the serial USB interface, if changable
+ * Currently only supported by DUE platform
+ */
+// #define USB_DEVICE_VENDOR_ID           0x0000
+// #define USB_DEVICE_PRODUCT_ID          0x0000
+// #define USB_DEVICE_MANUFACTURE_NAME    WEBSITE_URL
